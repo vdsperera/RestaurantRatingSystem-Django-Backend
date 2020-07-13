497a8b611cecee21f7d4e1dfc04c9281610724bb
vidumini <heartyvds@gmail.com>
2020-07-13 08:17:13 +0530

    added method definitions for restaurant service class

---
+++ restaurantratingapi/services/RestaurantService.py
@@ -0,0 +1,21 @@
+class RestaurantService:
+
+	def register_restaurant():
+		
+
+	def de_register_restaurant():
+
+
+	def get_restaurant():
+
+
+	def get_restaurant_list():
+
+
+	def request_edit():
+
+
+	def approve_edit():
+
+
+	def claim_restaurant():
