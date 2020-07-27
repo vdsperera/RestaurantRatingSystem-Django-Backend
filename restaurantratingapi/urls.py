5e331d778a38bc6692ce41c24ab6c233aba3fd0e
vidumini <heartyvds@gmail.com>
2020-07-27 12:23:57 +0530

    added restaurant urls to test

--- restaurantratingapi/urls.py
+++ restaurantratingapi/urls.py
@@ -5,10 +5,18 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'roles', views.UserRoleViewSet)
+router.register(r'restaurants', views.RestaurantViewSet, basename='RestaurantView')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    # path('restaurants/', views.RestaurantViewSet.as_view(), name='RestaurantView'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+# POST  /api/restaurants
+# PATCH /api/restaurants/[id]
+# PUT   /api/restaurants/[id]
+# GET   /api/restaurants/[id]
+# GET   /api/RestaurantViewSet
