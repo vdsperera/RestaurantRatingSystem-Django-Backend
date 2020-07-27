b7af2c7e196fd7760d4d91ccf715e3eca22c8abf
vidumini <heartyvds@gmail.com>
2020-07-27 12:24:24 +0530

    added restaurant serializer

--- restaurantratingapi/serializers.py
+++ restaurantratingapi/serializers.py
@@ -1,7 +1,13 @@
 from .models import UserRole
+from .models import Restaurant
 from rest_framework import serializers
 
 class UserRoleSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = UserRole
 		fields = ('role_id', 'role_name')
+
+class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Restaurant
+		fields = ('restaurant_id', 'name', 'address', 'longitude','latitude', 'phone_number', 'claimed', 'claimed_by', 'code', 'created_by')
