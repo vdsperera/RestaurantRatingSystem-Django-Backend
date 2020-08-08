5ed9a7c3f6d74b641c178d3cf28916f1108e15d7
vidumini <heartyvds@gmail.com>
2020-08-08 20:14:05 +0530

    fixed coors issue

--- restaurantratingsystem/settings.py
+++ restaurantratingsystem/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'restaurantratingapi.apps.RestaurantratingapiConfig',
     'rest_framework',
     'django.contrib.gis',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
