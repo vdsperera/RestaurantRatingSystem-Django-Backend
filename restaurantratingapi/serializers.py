ea4cb8e69e8305cccad63a61943f6fb74745e18a
vidumini <heartyvds@gmail.com>
2020-08-08 20:12:21 +0530

    updated serializer

--- restaurantratingapi/serializers.py
+++ restaurantratingapi/serializers.py
@@ -1,13 +1,21 @@
 from .models import UserRole
 from .models import Restaurant
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from .serializers import *
 
 class UserRoleSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = UserRole
-		fields = ('role_id', 'role_name')
+		fields = ['role_id', 'role_name']
 
-class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
+class RestaurantSerializer(serializers.ModelSerializer):
+	# created_by = UserSerializer(many=True, read_only=True)
 	class Meta:
 		model = Restaurant
-		fields = ('restaurant_id', 'name', 'address', 'longitude','latitude', 'phone_number', 'claimed', 'claimed_by', 'code', 'created_by')
+		fields = ['restaurant_id', 'name', 'address', 'longitude','latitude', 'phone_number', 'code','claimed', 'claimed_by', 'created_by']
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = User
+		fields = ['id', 'username']
