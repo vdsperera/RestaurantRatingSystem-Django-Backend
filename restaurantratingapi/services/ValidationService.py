13009f6bf56351b67e543c5416908db268c6c195
vidumini <heartyvds@gmail.com>
2020-08-08 20:11:27 +0530

    set validation service
    *geo coordination validation
    *phone number validation

--- restaurantratingapi/services/ValidationService.py
+++ restaurantratingapi/services/ValidationService.py
@@ -1,3 +1,5 @@
+import re
+
 class ValidationService:
 
     def __init__(self):
@@ -12,3 +14,18 @@ class ValidationService:
                 return False
         else:
             return False
+
+    @staticmethod
+    def is_geo_coordinate(value, type):
+        status = False
+        if type == 'lo':
+            return True if (value <= 90 and value >= -90) else False
+        if type == 'la':
+            return True if (value <= 180 and value >= -180) else False
+
+    @staticmethod
+    def is_phone_number(value):
+        if(re.match(r"^\+?1?\d{9,15}$", value)):
+            return True
+        else:
+            False
