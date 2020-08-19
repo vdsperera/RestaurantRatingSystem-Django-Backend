e22ecd6dd306ffcb1bf4b5f6906a1e3b92366582
vdsperera <heartyvds@gmail.com>
2020-08-19 23:14:21 +0530

    removed geo

--- restaurantratingsystem/settings.py
+++ restaurantratingsystem/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurantratingapi.apps.RestaurantratingapiConfig',
     'rest_framework',
-    'django.contrib.gis',
     'corsheaders'
 ]
 
