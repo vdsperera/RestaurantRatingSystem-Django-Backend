b1b3965878ee623b06ea4d43fa0db7ef0bfb30b6
vidumini <heartyvds@gmail.com>
2020-10-31 16:39:38 +0530

    set urls

--- restaurantratingapi/urls.py
+++ restaurantratingapi/urls.py
@@ -5,15 +5,23 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'roles', views.UserRoleViewSet)
-router.register(r'restaurants', views.RestaurantViewSet, basename='RestaurantView')
-router.register(r'ratings', views.RatingViewSet, basename='RatingView')
+# router.register(r'restaurants', views.RestaurantViewSet, basename='RestaurantView')
+# router.register(r'ratings', views.RatingViewSet, basename='RatingView')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    # Restaurants
     path('restaurants/<int:rest_id>/', views.RestaurantViewSet.as_view({'get':'retrieve'}), name='RestaurantView'),
-    path('ratings/<int:dish_id>/', views.RatingViewSet.as_view({'get':'retrieve'}), name='RatingView'),
+
+    # Ratings
+    path('ratings/', views.RatingViewSet.as_view({'post': 'create'}), name='RatingView'),
+    path('ratings/verify/<int:rating_id>/', views.RatingViewSet.as_view({'post':'verify'}), name='RatingView'),
+    path('ratings', views.RatingViewSet.as_view({'get':'retrieve'}), name='RatingView'),
+    # path('ratings/<int:rest_id>/', views.RatingViewSet.as_view('get':'retrieve'), name='RatingView')
+    path('ratings/list', views.RatingViewSet.as_view({'get':'list'}), name='RatingView'),
+    path('ratings/dishes', views.RatingViewSet.as_view({'get':'dish_list'}), name='RatingView'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
