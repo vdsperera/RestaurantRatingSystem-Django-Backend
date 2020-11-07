6f12abb57d66648c93a45e37d591a53bb72c5215
vidumini <heartyvds@gmail.com>
2020-11-07 18:24:03 +0530

    commented GEOS in database model

--- restaurantratingapi/models.py
+++ restaurantratingapi/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.gis.geos import Point
+# from django.contrib.gis.geos import Point
 from django.core.validators import RegexValidator
 from phonenumber_field.modelfields import PhoneNumberField
 
