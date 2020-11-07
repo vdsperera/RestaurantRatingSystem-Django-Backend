035255e9c4b830c94975a900ad6178ca3b2910d2
vidumini <heartyvds@gmail.com>
2020-11-07 18:26:14 +0530

    added service method for getting restaurant list

--- restaurantratingapi/services/RestaurantService.py
+++ restaurantratingapi/services/RestaurantService.py
@@ -200,7 +200,52 @@ class RestaurantService:
 
         return resp;
 
-    def get_restaurant_list():
+    def get_restaurant_list(self, data):
+        # return 'get restaurant list'
+        # rest = Restaurant.objects.get()
+
+        restaurant_list = Restaurant.objects.raw("""
+            SELECT *
+            FROM restaurant
+            """)
+
+        list = []
+        for rest in restaurant_list:
+            rating_svc = RatingService
+            rating_resp = rating_svc.get_ratings_for_restaurant(self, rest.restaurant_id)
+            print(rest.restaurant_id)
+            restaurant_model = {
+                "restaurant_id": rest.restaurant_id,
+                "restaurant_name": rest.name,
+                "address": rest.address,
+                "logitude": rest.longitude,
+                "latitude": rest.latitude,        
+                "phone_number": rest.phone_number,
+                "added_by": rest.created_by.id,
+                "claimed_by": rest.claimed_by,
+                "code": rest.code,
+                "claimed_status": ClaimStatus(rest.claimed).name,
+                "created_on": rest.created_on,
+                "total_no_of_ratings": rating_resp['data']['total_no_of_ratings'],
+                "dish_rating": rating_resp['data']['dish_rating'],
+                "price_rating": rating_resp['data']['price_rating'],
+                "service_rating": rating_resp['data']['service_rating'],
+                "overall_rating": rating_resp['data']['overall_rating']
+            }
+            list.append(restaurant_model)
+
+        # rating_svc = RatingService
+        # rating_resp = rating_svc.get_ratings_for_restaurant(self, data)
+
+        resp = {
+            "success": True,
+            "code": 200,
+            "message": "success GetRestaurantList",
+            "data": {
+                "restaurant_list": list
+            }
+        }
+        return resp
         pass
 
     def request_edit():
