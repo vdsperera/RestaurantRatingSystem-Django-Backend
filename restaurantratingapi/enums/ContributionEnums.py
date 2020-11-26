8e2f99aa9cb6d477a5d1aeaded7a129ef3574ac8
vidumini <heartyvds@gmail.com>
2020-11-26 20:56:06 +0530

    added contribution enums

---
+++ restaurantratingapi/enums/ContributionEnums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class ContributionTypes(Enum):
+	CustomerRegistration = 0
+	AddRestaurant = 1
+	AddUnverifiedRestaurantRating = 2
+	AddVerifiedRestaurantRatingg = 3
+	AddUnverifiedDishRating = 4
+	AddVerifiedDishRating = 5
+
+
