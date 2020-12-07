dda92c7d2d750bd1de342e1b2d6fb947ffa173c9
vidumini <heartyvds@gmail.com>
2020-12-07 18:41:14 +0530

    added restaurant component enum

--- restaurantratingapi/enums/RestaurantEnums.py
+++ restaurantratingapi/enums/RestaurantEnums.py
@@ -4,4 +4,12 @@ class ClaimStatus(Enum):
     Unclaimed = 0
     Pending = 1
     Claimed = 2
-    
+
+
+class RestaurantComponents(Enum):
+	RestaurantName = 1
+	RestaurantPhoneNumber = 2
+	RestaurantWebsite = 3
+	RestaurantEmail = 4
+	RestaurantAddress = 5
+	RestaurantLocation = 6
