13063100ed7b1013095c5124fdd06703f0c6f313
vidumini <heartyvds@gmail.com>
2020-12-26 11:46:18 +0530

    db backup

---
+++ DB/20201125/custom_user_202012241238.sql
@@ -0,0 +1,16 @@
+INSERT INTO rrs2.custom_user (user_id,account_status,email_code,email_verified,birthday,gender,address,phone_number,profile_picture,updated_on,level_number,role_id,updated_by) VALUES 
+(1,1,'#3456',1,'1996-01-11',0,'Ampegama','0770273653',NULL,NULL,0,3,NULL)
+,(2,1,'@4578',1,'1993-02-05',0,'Hikkaduwa','0718812099',NULL,NULL,1,1,NULL)
+,(3,1,'#4589',1,'1993-04-20',0,'Mitiyagoda','0715634785',NULL,NULL,1,2,NULL)
+,(4,1,'@4568',1,'1993-05-03',0,'Batapola','0717852635',NULL,NULL,2,2,NULL)
+,(5,1,'@3245',1,'1992-06-09',0,'Baddegama','0714562535',NULL,NULL,2,2,NULL)
+,(6,1,'@4354',1,'1993-08-07',0,'Batapola','0714893562',NULL,NULL,2,2,NULL)
+,(7,1,'@5487',1,'1993-02-03',0,'Hikkaduwa','0725689752',NULL,NULL,2,2,NULL)
+,(8,1,'@3444',1,'1995-02-12',0,'Panadura','0715678952',NULL,NULL,1,2,NULL)
+,(9,1,'@6745',1,'1994-06-15',0,'Gampaha','0725876953',NULL,NULL,1,2,NULL)
+,(10,1,'@2222',1,'1996-02-19',0,'Kalutara','0728952423',NULL,NULL,1,2,NULL)
+;
+INSERT INTO rrs2.custom_user (user_id,account_status,email_code,email_verified,birthday,gender,address,phone_number,profile_picture,updated_on,level_number,role_id,updated_by) VALUES 
+(11,1,'@5678',1,'1996-08-08',0,'Wadduwa','0715647382',NULL,NULL,1,3,NULL)
+,(12,1,'@3452',1,'1995-06-23',0,'Baddegama','0714858549',NULL,NULL,1,3,NULL)
+;
